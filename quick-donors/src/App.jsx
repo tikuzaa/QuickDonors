aacef20b76ca0b71362c307aaf78fd9ae77795c0
tikuzaa <dey.debangan2004@gmail.com>
2025-03-12 12:02:45 +0530

    init

---
+++ quick-donors/src/App.jsx
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Dashboard from "./pages/Dashboard";
+// import FindDonors from "./pages/FindDonors";
+// import BloodRequest from "./pages/BloodRequest";
+// import Notifications from "./pages/Notifications";
+// import Profile from "./pages/Profile";
+// import AdminPanel from "./pages/AdminPanel";
+// import NotFound from "./pages/NotFound"; // 404 Page
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        {/* <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/find-donors" element={<FindDonors />} />
+        <Route path="/blood-request" element={<BloodRequest />} />
+        <Route path="/notifications" element={<Notifications />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="*" element={<NotFound />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ quick-donors/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+const Login = () => {
+    return (
+    <p class="text-blue-500 text-3xl font-bold underline">Login</p>
+);
+  };
+  
+  export default Login;
