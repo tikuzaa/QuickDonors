208ad0605622be6b7c8a6f34c850ff429c4296c9
tikuzaa <dey.debangan2004@gmail.com>
2025-03-15 01:19:45 +0530

    landing page updated

--- quick-donors/src/components/Testimonials.jsx
+++ quick-donors/src/components/Testimonials.jsx
@@ -1,172 +1,144 @@
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { Reveal } from "./Reveal";
+import React, { useState, useEffect } from "react";
 
-const organizersData = [
-  {
-    name: "Shekhar Hans",
-    image: "/pfp/shekhar.jpg",
-    post: "WebDev Lead",
-  },
-  {
-    name: "Aniruddha Ghosh",
-    image: "/pfp/aniruddha.jpg",
-    post: "Admin Head",
-  },
-  {
-    name: "Arnab Manna",
-    image: "/pfp/arnab.jpg",
-    post: "CDP Lead",
-  },
-  {
-    name: "Bidisha Banik",
-    image: "/pfp/bidisha.jpg",
-    post: "Site Developer, Repo Owner",
-  },
-  {
-    name: "Debangan Dey",
-    image: "/pfp/debangan.jpg",
-    post: "Site Developer, Repo Owner",
-  },
-  {
-    name: "Sayandip Ghosh",
-    image: "/pfp/sayandip.jpg",
-    post: "Project Manager, Repo Owner",
-  },
-  {
-    name: "Sayan Sanpui",
-    image: "/pfp/sayan.jpg",
-    post: "Repo Owner",
-  },
-  {
-    name: "Sahil Golder",
-    image: "/pfp/sahil.jpg",
-    post: "Repo Owner",
-  },
-  {
-    name: "Somshirsha Bhattacharya",
-    image: "/pfp/somshirsha.jpg",
-    post: "Repo Owner",
-  },
-  {
-    name: "Ankit Kundu",
-    image: "/pfp/ankit.jpg",
-    post: "Repo Owner",
-  },
-  {
-    name: "Aahana Malakar",
-    image: "/pfp/aahana.jpg",
-    post: "PR Manager",
-  },
-  {
-    name: "Sagarika Bandyopadhyay",
-    image: "/pfp/sagarika.jpg",
-    post: "PR Manager",
-  },
-  {
-    name: "Muskaan Das",
-    image: "/pfp/muskaan.jpg",
-    post: "Repo Owner",
-  },
+const testimonialList = [
+	[
+		{
+			img: "https://cdn.easyfrontend.com/pictures/testimonial/testimonial_square_1.jpeg",
+			name: "Aksay Kumar",
+			position: "CEO & Founder at EasyFrontend",
+			content:
+				"It's easier to reach your savings goals when you have the right savings account. Take a look and find the right one for you.",
+		},
+		{
+			img: "https://cdn.easyfrontend.com/pictures/testimonial/testimonial_square_2.jpeg",
+			name: "Sara Tailor",
+			position: "CEO & Founder at EasyFrontend",
+			content:
+				"It's easier to reach your savings goals when you have the right savings account. Take a look and find the right one for you.",
+		},
+		{
+			img: "https://cdn.easyfrontend.com/pictures/testimonial/testimonial_square_3.jpeg",
+			name: "John Leo",
+			position: "CEO & Founder at EasyFrontend",
+			content:
+				"It's easier to reach your savings goals when you have the right savings account. Take a look and find the right one for you.",
+		},
+	],
+	[
+		{
+			img: "https://cdn.easyfrontend.com/pictures/testimonial/testimonial_square_2.jpeg",
+			name: "Aksay Kumar",
+			position: "CEO & Founder at EasyFrontend",
+			content:
+				"It's easier to reach your savings goals when you have the right savings account. Take a look and find the right one for you.",
+		},
+		{
+			img: "https://cdn.easyfrontend.com/pictures/testimonial/testimonial_square_3.jpeg",
+			name: "Sara Tailor",
+			position: "CEO & Founder at EasyFrontend",
+			content:
+				"It's easier to reach your savings goals when you have the right savings account. Take a look and find the right one for you.",
+		},
+		{
+			img: "https://cdn.easyfrontend.com/pictures/testimonial/testimonial_square_1.jpeg",
+			name: "John Leo",
+			position: "CEO & Founder at EasyFrontend",
+			content:
+				"It's easier to reach your savings goals when you have the right savings account. Take a look and find the right one for you.",
+		},
+	],
+	[
+		{
+			img: "https://cdn.easyfrontend.com/pictures/testimonial/testimonial_square_3.jpeg",
+			name: "Aksay Kumar",
+			position: "CEO & Founder at EasyFrontend",
+			content:
+				"It's easier to reach your savings goals when you have the right savings account. Take a look and find the right one for you.",
+		},
+		{
+			img: "https://cdn.easyfrontend.com/pictures/testimonial/testimonial_square_2.jpeg",
+			name: "Sara Tailor",
+			position: "CEO & Founder at EasyFrontend",
+			content:
+				"It's easier to reach your savings goals when you have the right savings account. Take a look and find the right one for you.",
+		},
+		{
+			img: "https://cdn.easyfrontend.com/pictures/testimonial/testimonial_square_1.jpeg",
+			name: "John Leo",
+			position: "CEO & Founder at EasyFrontend",
+			content:
+				"It's easier to reach your savings goals when you have the right savings account. Take a look and find the right one for you.",
+		},
+	],
 ];
 
-const CustomPrevArrow = ({ onClick }) => {
-  return (
-    <div
-      className="absolute left-0 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer"
-      onClick={onClick}
-    >
-      <button className="bg-[#00749d] text-white rounded-full p-2 m-8 w-12 h-12">
-        &#10094; {/* Left arrow symbol */}
-      </button>
-    </div>
-  );
-};
-
-const CustomNextArrow = ({ onClick }) => {
-  return (
-    <div
-      className="absolute right-0 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer"
-      onClick={onClick}
-    >
-      <button className="bg-[#00749d] text-white rounded-full p-2 m-8 w-12 h-12">
-        &#10095; {/* Right arrow symbol */}
-      </button>
-    </div>
-  );
-};
-
 const Testimonials = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    cssEase: "linear",
-    prevArrow: <CustomPrevArrow />,
-    nextArrow: <CustomNextArrow />,
-    responsive: [
-      {
-        breakpoint: 1200, // For screens up to 640px
-        settings: {
-          slidesToShow: 2, // Show 2 slides
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 800, // For screens up to 500px
-        settings: {
-          slidesToShow: 1, // Show 1 slide
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+	const [index, setIndex] = useState(0);
 
-  return (
-    <div className="min-h-screen organizers-section relative py-40 px-10 bg-[#ffffff]">
-      <div className="absolute z-4 -rotate-[20deg] h-[355px] w-[355px] -left-[3rem] -top-[12rem]">
-      <Reveal> <img src="/pfp/calendar.png" alt=".." />   </Reveal>
-      </div>
-      <h2 className="text-center font-oxanium text-[#003c54] text-8xl font-bold mb-[8rem] max-[640px]:text-5xl md:text-6xl lg:text-8xl">
-        <Reveal>Meet Our Organizers</Reveal>
-      </h2>
+	// Auto slide effect
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setIndex((prevIndex) => (prevIndex + 1) % testimonialList.length);
+		}, 3000); // Change slides every 3 seconds
 
-      <Reveal>
-        <Slider {...settings} className="custom-dots">
-          {organizersData.map((organizer, index) => (
-            <div key={index} className="p-4">
-            <div className="flex justify-center items-center flex-wrap">
-              <div className="relative w-[350px] h-[350px] m-[2.5rem] group">
-                <div className="absolute top-0 left-0 w-full h-full z-20 transition-transform duration-500 ease-in-out transform group-hover:-translate-y-14">
-                  <img
-                    className="w-full h-full object-cover rounded-[1.5rem]"
-                    src={organizer.image}
-                    alt={organizer.name}
-                  />
-                </div>
-                <div className="bg-gray-200 rounded-[1.5rem] absolute top-0 left-0 w-full h-full p-6 flex justify-center items-end z-10 transition-transform duration-500 ease-in-out transform group-hover:translate-y-14 text-center">
-                  <h2 className="text-2xl text-gray-900 font-medium leading-8">
-                    {organizer.name}
-                    <br />
-                    <span className="text-gray-600 text-lg font-light tracking-wide">
-                    {organizer.post}
-                    </span>
-                  </h2>
-                </div>
-              </div>
-            </div>
-          </div>
-          ))}
-        </Slider>
-      </Reveal>
-    </div>
-  );
+		// Cleanup function to clear the interval
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<section className="ezy__testimonial24 light py-14 md:py-24 bg-white dark:bg-[#0b1727] text-zinc-900 dark:text-white">
+			<div className="container px-4 mx-auto">
+				<div className="flex items-center justify-center text-center mb-6 md:mb-12">
+					<div className="max-w-xl">
+						<hr className="w-20 mb-4 border-gray-300 dark:border-gray-600 mx-auto" />
+						<h2 className="text-[32px] font-bold">What They Say</h2>
+					</div>
+				</div>
+
+				<div>
+					<div className="grid grid-cols-3 gap-6">
+						{testimonialList[index].map((testimonial, i) => (
+							<div className="col-span-3 lg:col-span-1" key={i}>
+								<div className="h-full p-6 xl:p-10">
+									<div className="flex items-center mb-6">
+										<div className="mr-3">
+											<img
+												src={testimonial.img}
+												alt={testimonial.name}
+												className="max-w-full h-auto rounded-full border"
+												width="85"
+											/>
+										</div>
+										<div>
+											<h4 className="text-xl font-medium">
+												{testimonial.name}
+											</h4>
+											<p className="text-sm mb-2">{testimonial.position}</p>
+										</div>
+									</div>
+									<p className="opacity-75 mb-2">{testimonial.content}</p>
+								</div>
+							</div>
+						))}
+					</div>
+
+					<div className="flex justify-center gap-3 m-0 mt-12">
+						{testimonialList.map((_, i) => (
+							<button
+								className={`w-12 h-[3px] transition-transform ${
+									index === i
+										? "scale-125 bg-blue-600"
+										: " bg-gray-400 dark:bg-slate-800"
+								} `}
+								key={i}
+								onClick={() => setIndex(i)}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Testimonials;
--- quick-donors/src/pages/Home.jsx
+++ quick-donors/src/pages/Home.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Works from "../components/Works"
 import Testimonials from "../components/Testimonials"
 import Navbar from "../components/Navigation";
-import Footer from "../components/Footer/Footer";
+import Footer from "../components/Footer";
+import { Button } from "@material-tailwind/react";
 import './style.scss'
 
 
@@ -29,21 +30,22 @@ const Home = () => {
     <Navbar isScrolled={isScrolled}></Navbar>
       <div className="relative -translate-y-23">
         <div className="relative">     
-        <div class="text-container flex flex-row justify-between items-center w-full">
+        <div class="text-container flex flex-col 2xl:flex-row justify-between items-center w-full">
           <div className=" flex flex-col p-4 max-w-200">
-          <div>Quick Donor</div>
-          <div className='text-xl'>
+          <div className='text-[60px] md:text-[96px]'>QuickDonor</div>
+          <div className='text-[16px] md:text-[20px]'>
           Welcome to our all new Digital Blood Bank. We instantly connect donors with those in urgent need, 
           ensuring life-saving blood reaches the right people at the right time.
           <p>
           Join our network of lifesavers and help those in urgent need. Find donors or donate blood in just a few clicks.
           </p>
           </div>
-          Join Now
+          <Button size="lg" className='bg-[#E32636] hover:bg-[#E32636]/70 h-10 w-30 mt-2 text-[16px] md:text-[18px] font-medium'>Join Now</Button>
+          {/* <p className='text-2xl'>Join Now</p> */}
 
           </div>
     <div>
-    <img className="max-w-200" src="/hand_hold.png" alt="" />
+    <img className="w-full md:max-w-200 " src="/hand_hold.png" alt="" />
 
     </div>
   </div>   
