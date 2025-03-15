9d1b1dc6bfe5e1a63a04aa13fdae425d6c00bae2
Binay <binaydas1123@gmail.com>
2025-03-15 22:31:09 +0530

    Update profile

--- quick-donors/src/components/signup-form.jsx
+++ quick-donors/src/components/signup-form.jsx
@@ -11,6 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function SignupForm({ className, ...props }) {
   const [username, setUsername] = useState("");
@@ -22,6 +23,7 @@ export function SignupForm({ className, ...props }) {
   const [address, setAddress] = useState("");
 
   const bloodGroups = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
+  const navigate = useNavigate();
 
   const handleSignUp = async (event) => {
     event.preventDefault(); // Prevent page refresh on form submission
@@ -41,6 +43,7 @@ export function SignupForm({ className, ...props }) {
         address,
       });
       console.log("Signup successful:", response.data);
+      navigate('/login');
     } catch (error) {
       console.error("Signup error:", error.response?.data || error.message);
       alert("Signup failed. Please try again.");
